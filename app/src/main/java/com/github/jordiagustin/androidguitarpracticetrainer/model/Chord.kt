ef5f24152a9a77c7a7514248fb947af6072dec40
Jordi <jordiagustin@gmail.com>
2026-07-17 13:46:05 +0200

    Definir Chord

--- app/src/main/java/com/github/jordiagustin/androidguitarpracticetrainer/model/Chord.kt
+++ app/src/main/java/com/github/jordiagustin/androidguitarpracticetrainer/model/Chord.kt
@@ -1,2 +1,11 @@
 package com.github.jordiagustin.androidguitarpracticetrainer.model
 
+data class Chord (
+    val name: String,
+    val type: ChordType
+)
+
+enum class ChordType{
+    MAJOR,
+    MINOR
+}
